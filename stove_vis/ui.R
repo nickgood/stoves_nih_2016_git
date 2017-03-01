e870277ecd35e37645c336aa3099acc39a46b8fc
ngood13 <ngood@colostate.edu>
2017-03-01 15:04:44 -0700

    app layout updated

--- stove_vis/ui.R
+++ stove_vis/ui.R
@@ -1,23 +1,28 @@
 library(shiny)
+library(shinythemes)
 
 # define UI
 shinyUI(fluidPage(
- # title
-  titlePanel("Plot stove emissions"),
 
- # sidebar
-  sidebarLayout(
-    sidebarPanel(
-      selectInput('x_var', 'x axis:', colnames(df)),
-      selectInput('y_var', 'y axis:', colnames(df)),
-      selectInput('p_col', 'color by:', colnames(df)),
-      selectInput('p_facet', 'facet by:', colnames(df)),
-      selectInput('p_filter', 'filter by:', colnames(df)),
-      checkboxInput("p_bad","exclude bad data")
+  navbarPage(theme = shinytheme("united"), "STOVES",
+
+    tabPanel("Summary"),
+
+    tabPanel("Compare Pollutants",
+      sidebarLayout(
+        sidebarPanel(
+          selectInput('x_var', 'x axis:', colnames(df)),
+          selectInput('y_var', 'y axis:', colnames(df)),
+          selectInput('p_col', 'color by:', colnames(df)),
+          selectInput('p_facet', 'facet by:', colnames(df)),
+          selectInput('p_filter', 'filter by:', colnames(df)),
+          checkboxInput("p_bad","exclude bad data")
+        ),
+        mainPanel(
+          plotOutput("stove_plot")
+       )
+      )
     ),
- # show plot
-  mainPanel(
-    plotOutput("stove_plot")
-  )
+    tabPanel("About")
   )
 ))
