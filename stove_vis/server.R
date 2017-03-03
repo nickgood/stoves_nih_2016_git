9be7741e4a99c1df414d8f811d126ed367fb24a4
ngood13 <ngood@colostate.edu>
2017-03-03 12:16:47 -0700

    fix reactivity from update button

--- stove_vis/server.R
+++ stove_vis/server.R
@@ -7,44 +7,58 @@
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, clientData, session){
-
+	
+#_______________________________________________________________________________
+	# plot one pollutant versus another
+	stove_plot <- eventReactive(input$updateButton, 
+  { 		d_left <- dplyr::filter_(df, ~pol == input$y_pol, ~units ==  input$y_metric)
+  d_bottom <- dplyr::filter_(df, ~pol == input$x_pol, ~units ==  input$x_metric)
+  p_data <- dplyr::inner_join(d_left, d_bottom, by = "id")
+  
+  p <- ggplot(p_data, aes(value.x, value.y)) +
+  	geom_point() +
+  	theme_minimal()
+  print(p)
+	})
+#_______________________________________________________________________________	
+	
+	output$stove_plot <- renderPlot({
+		stove_plot()
+	})
+	
+	
   observe({
-  # pollutant list to display - left axis
+#_______________________________________________________________________________
+# pollutant list to display - left axis
    inst_y <- input$y_inst
    inst_x <- input$x_inst
+#_______________________________________________________________________________
 
-    # plot one pollutant versus another
-      output$stove_plot <- eventReactive(input$updateButton, 
-                           {renderPlot({
-                           # get data
-                           p <- ggplot(df, aes_string(input$x_var, input$y_var, colour = input$p_col)) +
-                                  geom_line() +
-                                  theme_minimal() +
-                                  facet_wrap(as.character(input$p_facet), scales = "free", ncol = 1) +
-                                  theme(legend.position = "right") +
-                                  theme(axis.text.x = element_text(angle = 60, hjust = 1))
-                            print(p)
-                         })
-      })
-      
+#_______________________________________________________________________________
  # update y axis pol menu based on instrument selection
   if(inst_y == "carbs"){
-  	inst_y <- pol_carb
+    inst_y <- pol_carb
   }else if(inst_y == "ecoc"){
-  	inst_y <- pol_ecoc
+    inst_y <- pol_ecoc
   }else if(inst_y == "fivegas"){
-  	inst_y <-  pol_fivegas
+    inst_y <-  pol_fivegas
+  }else if(inst_y == "grav"){
+  	inst_y <-  pol_grav  
   }else if(inst_y == "ions"){
-  	inst_y <-  pol_ions
+    inst_y <-  pol_ions
   }else if(inst_y == "voc"){
-  	inst_y <-  pol_voc
+    inst_y <-  pol_voc
   }else(inst_y <-  "select instrument")
+#_______________________________________________________________________________
 
+#_______________________________________________________________________________
  # update y axis pollutant menu
   updateSelectInput(session,
                     "y_pol",
                      choices = inst_y)
+#_______________________________________________________________________________
 
+#_______________________________________________________________________________
   # update x axis pol menu based on instrument selection
   if(inst_x == "carbs"){
   	inst_x <- pol_carb
@@ -52,16 +66,20 @@ shinyServer(function(input, output, clientData, session){
   	inst_x <- pol_ecoc
   }else if(inst_x == "fivegas"){
   	inst_x <-  pol_fivegas
+  }else if(inst_x == "grav"){
+  	inst_x <-  pol_grav
   }else if(inst_x == "ions"){
   	inst_x <-  pol_ions
   }else if(inst_x == "voc"){
   	inst_x <-  pol_voc
   }else(inst_x <-  "select instrument")
-  
+#_______________________________________________________________________________
+
+#_______________________________________________________________________________
  # update x axis pollutant menu
   updateSelectInput(session,
                     "x_pol",
                     choices = inst_x)
-
   })
 })
+#_______________________________________________________________________________
