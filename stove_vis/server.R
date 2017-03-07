ee916242ee77a0a9532ce9752acaaa6baf5dd8ca
kbilsback <kbilsbac@rams.colostate.edu>
2017-03-07 11:01:40 -0700

    revert to master

--- stove_vis/server.R
+++ stove_vis/server.R
@@ -7,13 +7,13 @@ shinyServer(function(input, output){
 
  # plot one pollutant versus another
   output$stove_plot <- renderPlot({
-p <- ggplot(df, aes_string(input$x_var, input$y_var, colour = input$p_col)) +
-  geom_line() +
-  theme_minimal() +
-  facet_wrap(as.character(input$p_facet), scales = "free", ncol = 1) +
-  theme(legend.position = "right") +
-  theme(axis.text.x = element_text(angle = 60, hjust = 1))
-print(p)
+    p <- ggplot(df, aes_string(input$x_var, input$y_var, colour = input$p_col)) +
+      geom_line() +
+      theme_minimal() +
+      facet_wrap(as.character(input$p_facet), scales = "free", ncol = 1) +
+      theme(legend.position = "right") +
+      theme(axis.text.x = element_text(angle = 60, hjust = 1))
+    print(p)
   })
 
  # plot 
